4c4fca3239738ad8ac3213bf9690d9bd23f93959
william <williamrmoore93@gmail.com>
2023-07-30 18:49:40 -0400

    rollback testing

--- src/Tests/PeerJsTest.ts
+++ src/Tests/PeerJsTest.ts
@@ -1,7 +1,9 @@
 import { DataConnection, Peer } from 'peerjs';
 import { keys } from '../input/SimpleInput';
 
-let remoteInputBuffer = Array<KeyInput>();
+let remoteInputBuffer = Array<FrameInput>();
+let localInputBuffer = Array<KeyInput>();
+
 const peer = new Peer();
 
 peer.on('open', (id) => {
@@ -26,12 +28,13 @@ document.getElementById('connectgame').addEventListener('click', () => {
 
 function BeginHost(localPeer: Peer) {
   localPeer.on('connection', (c) => {
-    c.on('open', () => {
-      c.send('Hello from Host');
-    });
+    // c.on('open', () => {
+    //   c.send('Hello from Host');
+    // });
 
     c.on('data', (remoteData) => {
-      console.log(remoteData);
+      //console.log(remoteData);
+      AddRemoteInput(remoteData as KeyInput);
     });
     SetUpDataSendLoop(c);
   });
@@ -43,13 +46,14 @@ function ConnectToRemotePeer(
 ): DataConnection {
   let connection = localPeer.connect(remotePeerId);
 
-  connection.on('open', () => {
-    connection.send('Hello Frome Client.');
-  });
+  // connection.on('open', () => {
+  //   connection.send('Hello Frome Client.');
+  // });
 
   connection.on('data', (remoteData) => {
     //let remoteInput = remoteData as KeyInput
-    console.log(remoteData);
+    //console.log(remoteData);
+    AddRemoteInput(remoteData as KeyInput);
   });
 
   return connection;
@@ -61,39 +65,43 @@ export function Run() {
   Init();
 }
 
-function Init() {
-  // connection.on('open', () => {
-  //   connection.send('hi');
-  //   //tick();
-  // });
-  //PeerInput();
-  //tick();
-}
+function Init() {}
 
 type KeyInput = {
   action: string;
   inputFrame: number;
 };
 
+type FrameInput = {
+  guessed: boolean;
+  keyInput: KeyInput;
+};
+
 function GetKey(inputFrame: number): KeyInput {
+  let localInput = {} as KeyInput;
+
   if (keys.d.pressed) {
-    return { action: 'd', inputFrame };
+    localInput = { action: 'd', inputFrame };
   } else if (keys.a.pressed) {
-    return { action: 'a', inputFrame };
+    localInput = { action: 'a', inputFrame };
   } else if (keys.w.pressed) {
-    return { action: 'w', inputFrame };
+    localInput = { action: 'w', inputFrame };
   } else if (keys.s.pressed) {
-    return { action: 's', inputFrame };
+    localInput = { action: 's', inputFrame };
   } else {
-    return { action: 'n/a', inputFrame };
+    localInput = { action: 'n/a', inputFrame };
   }
+
+  localInputBuffer[localInput.inputFrame] = localInput;
+
+  return localInput;
 }
 
 function SendDataToPeer(c: DataConnection, keyInput: any) {
   c.send(keyInput);
 }
 
-const FPS = 2;
+const FPS = 60;
 let now = {} as number;
 let then = Date.now();
 const interval = 1000 / FPS;
@@ -107,8 +115,75 @@ function SetUpDataSendLoop(c: DataConnection) {
 
   if (delta > interval) {
     let localInput = GetKey(frame);
+    let remoteInput = GetCurrentRemoteInput(frame);
+    console.log(remoteInput);
     SendDataToPeer(c, localInput);
     frame++;
     then = now - (delta % interval);
   }
+}
+
+function AddRemoteInput(input: KeyInput) {
+  let checkFrame = remoteInputBuffer[input.inputFrame];
+
+  if (checkFrame === undefined) {
+    remoteInputBuffer[input.inputFrame] = {
+      guessed: false,
+      keyInput: input,
+    } as FrameInput;
+    return;
+  }
+
+  if (checkFrame.guessed) {
+    if (CompareFrames(checkFrame, input)) {
+      remoteInputBuffer[input.inputFrame].guessed = false;
+      return;
+    }
+
+    console.log('INITIATE ROLLBACK!');
+  }
+}
+
+function CompareFrames(guessedFrame: FrameInput, realFrame: KeyInput) {
+  if (guessedFrame.keyInput.action === realFrame.action) {
+    return true;
+  }
+  return false;
+}
+
+function GetCurrentRemoteInput(f: number) {
+  let remoteInput = remoteInputBuffer[f];
+
+  if (remoteInput !== undefined) {
+    return remoteInput;
+  }
+
+  remoteInput = GetLastRemoteInput(f);
+  remoteInput.guessed = true;
+  remoteInputBuffer[f] = remoteInput;
+
+  console.log('guessed');
+
+  return remoteInput;
+}
+
+function GetLastRemoteInput(f: number) {
+  // debugger;
+  f--;
+
+  let remoteInput = remoteInputBuffer[f];
+
+  while (remoteInput === undefined && f >= 0) {
+    f--;
+    remoteInput = remoteInputBuffer[f];
+  }
+
+  if (remoteInput !== undefined) {
+    return remoteInput;
+  }
+
+  return {
+    guessed: true,
+    keyInput: { action: 'n/a', inputFrame: f },
+  } as FrameInput;
 }
