fcc7aa9e2313b779d1fd35b1f5e395b529fd9efa
william <williamrmoore93@gmail.com>
2023-10-14 18:11:34 -0400

    Added a collision system

---
+++ src/Game/Collision/StageCollisionSystem.ts
@@ -0,0 +1,41 @@
+import { Player } from '../Player/Player';
+import Stage from '../../classes/Stage';
+import { IntersectsPolygons } from '../../Physics/Collisions';
+import {
+  VectorAdder,
+  VectorMultiplier,
+  VectorNegator,
+} from '../../Physics/FlatVec';
+
+export class StageCollisionSystem {
+  private readonly players: Array<Player>;
+  private readonly stage: Stage;
+
+  constructor(players: Array<Player>, stage: Stage) {
+    this.players = players;
+    this.stage = stage;
+  }
+
+  public handle() {
+    const sverts = this.stage.GetVerticies();
+
+    for (let index = 0; index < this.players.length; index++) {
+      const player = this.players[index];
+      const pverts = player.ECB.GetVerticies();
+
+      const res = IntersectsPolygons(sverts, pverts);
+
+      if (res.collision) {
+        const move = VectorMultiplier(VectorNegator(res.normal!), res.depth!);
+
+        player.PlayerPosition = VectorAdder(player.PlayerPosition, move);
+
+        player.Grounded = true;
+      }
+
+      if (!res.collision) {
+        player.Grounded = false;
+      }
+    }
+  }
+}
